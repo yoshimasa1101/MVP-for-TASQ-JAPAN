36d884b0e7192d3e2fa09948970755ded75cf50e
yoshimasa1101 <yo50si11@gmail.com>
2025-09-15 22:13:40 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <div style={{ padding: 20 }}>
-      <h1>TASQ JAPAN MVP</h1>
-      <p>ようこそ！通常オークション / 逆オークションをテストできます。</p>
-      <a href="/auction">オークションページへ</a>
-    </div>
-  );
+    <main style={{ textAlign: 'center', padding: '40px' }}>
+      {/* ロゴ表示 */}
+      <Image
+        src="/logo.svg" // publicフォルダ直下のlogo.svgを参照
+        alt="TASQ Logo"
+        width={150}
+        height={150}
+        priority
+      />
+
+      {/* タイトルや説明 */}
+      <h1 style={{ marginTop: '20px' }}>ようこそ！複雑なワークション/オペレーションテストです。</h1>
+
+      {/* ナビゲーションリンク */}
+      <p style={{ marginTop: '20px' }}>
+        <a href="/nav" style={{ color: '#0070f3', textDecoration: 'underline' }}>
+          ナビゲーションページ
+        </a>
+      </p>
+    </main>
+  )
 }
